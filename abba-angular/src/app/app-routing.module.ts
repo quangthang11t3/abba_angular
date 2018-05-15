59cd5affb76b1cc27b09ab8e7810eadebf4e1e53
thangnq <thang.nq@globaldesignit.vn>
2018-05-15 10:59:28 +0700

    add login component

--- abba-angular/src/app/app-routing.module.ts
+++ abba-angular/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent }      from './home/home.component';
+import { LoginComponent }      from './login/login.component';
+import { DashboardComponent }      from './dashboard/dashboard.component';
+
+
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }   
 ];
 
 @NgModule({
--- abba-angular/src/app/app.module.ts
+++ abba-angular/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './dashboard/admin/admin.component';
+import { AdsComponent } from './dashboard/ads/ads.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    DashboardComponent,
+    LoginComponent,
+    AdminComponent,
+    AdsComponent
   ],
   imports: [
     BrowserModule,
